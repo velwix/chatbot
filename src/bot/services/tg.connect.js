39e89caa0b70d66008d1f82da3d39f309bf4676a
velwix <shokirjonurazimbetov3@gmail.com>
2026-05-10 22:09:10 +0500

    Update tg.connect.js

--- src/bot/services/tg.connect.js
+++ src/bot/services/tg.connect.js
@@ -1,15 +1,32 @@
-export default async function tg(env, method, payload = {}) {
-  const token = env.BOT_TOKEN;
+export const telegram = (env) => {
+  const baseUrl = `https://api.telegram.org/bot${env.BOT_TOKEN}`;
 
-  const url = `https://api.telegram.org/bot${token}/${method}`;
+  const call = async (method, params = {}) => {
+    const url = `${baseUrl}/${method}`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(params),
+    });
+    return response.json();
+  };
 
-  const res = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(payload)
-  });
+  return {
+    sendMessage: (chatId, text, extra = {}) => 
+      call("sendMessage", { chat_id: chatId, text, ...extra }),
+    
+    answerCallbackQuery: (callbackQueryId, text, showAlert = false) =>
+      call("answerCallbackQuery", { callback_query_id: callbackQueryId, text, show_alert: showAlert }),
+    
+    editMessageText: (chatId, messageId, text, extra = {}) =>
+      call("editMessageText", { chat_id: chatId, message_id: messageId, text, ...extra }),
+    
+    deleteMessage: (chatId, messageId) =>
+      call("deleteMessage", { chat_id: chatId, message_id: messageId }),
+    
+    sendPhoto: (chatId, photo, caption, extra = {}) =>
+      call("sendPhoto", { chat_id: chatId, photo, caption, ...extra }),
 
-  return await res.json();
-}
+    custom: (method, params) => call(method, params)
+  };
+};
