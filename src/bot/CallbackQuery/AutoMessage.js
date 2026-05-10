c0f577074dba1d0f841ee1ed0e104f726e62b97a
velwix <shokirjonurazimbetov3@gmail.com>
2026-05-10 22:58:17 +0500

    Create AutoMessage.js

---
+++ src/bot/CallbackQuery/AutoMessage.js
@@ -0,0 +1,54 @@
+import { telegram } from "../services/tg.connect.js";
+import { getDb } from "../database/db.connect.js";
+
+export default async function autoMessage(ctx, env) {
+  const chat_id = ctx.message.chat.id;
+  const user_id = ctx.from.id;
+  const callback_query_id = ctx.id;
+  
+  const bot = telegram(env);
+  const db = getDb(env);
+
+  await bot.answerCallbackQuery(callback_query_id);
+
+  const { data: settings } = await db.from('settings').select('auto_msg').eq('user_id', user_id).single();
+  const { data: msgData } = await db.from('message').select('auto_msg').eq('user_id', user_id).single();
+
+  if (settings && settings.auto_msg === true && msgData && msgData.auto_msg) {
+    return await bot.sendMessage(chat_id, 
+      `Sizda Avto Xabar yoniq✅\n` +
+      `Xabar matni:${msgData.auto_msg}`, 
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              { 
+                text: "Taxrirlash✏️", 
+                callback_data: "edit_auto_msg"
+style: "danger" 
+              }
+            ]
+          ]
+        }
+      }
+    );
+  } else {
+    return await bot.sendMessage(chat_id, 
+      `Sizda avto xabar qo'shilmagan!\n` +
+      `Qo'shish uchun pastdagi Tugmani bosing👇🏿`, 
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              { 
+                text: "Xabar qo'shish📧🔑", 
+                callback_data: "add_auto_msg"
+style: "succes" 
+              }
+            ]
+          ]
+        }
+      }
+    );
+  }
+}
