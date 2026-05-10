d282e7f95308e8725ccabe17d18689cd0f59923d
velwix <shokirjonurazimbetov3@gmail.com>
2026-05-10 17:41:59 +0500

    Update start.js

--- src/bot/commands/start.js
+++ src/bot/commands/start.js
@@ -1,45 +1,8 @@
 import tg from "../services/tg.connect.js";
-import checkSubscription from "../services/vw.connect.js";
 
 export default async function start(ctx, env) {
-
   const chat_id = ctx.chat.id;
-  const user_id = ctx.from.id;
   const username = ctx.from.username || "user";
-
-  const channelRow = await env.DB.prepare(
-    "SELECT username FROM channels LIMIT 1"
-  ).first();
-
-  const channel = channelRow?.username;
-
-  const sub = await checkSubscription(env, user_id, [channel]);
-  const isJoined = sub.results?.[channel] || false;
-
-  if (!isJoined) {
-    return await tg(env, "sendMessage", {
-      chat_id,
-      text: "Botimizdan to'liq foydalanish uchun kanallarimizga obuna bo'ling!✨️",
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: channel || "Kanalga obuna bo'ling",
-              url: channel ? `https://t.me/${channel.replace("@", "")}` : "https://t.me/",
-              style: "danger"
-            }
-          ],
-          [
-            {
-              text: "Obunamni tekshirish✅️",
-              callback_data: "check_sub",
-              style: "success"
-            }
-          ]
-        ]
-      }
-    });
-  }
 
   return await tg(env, "sendMessage", {
     chat_id,
